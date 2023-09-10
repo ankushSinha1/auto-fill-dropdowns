4f9c2c548195a723d8ed95e0e4cc0192aa5eb78a
Ankush Sinha <97420654+ankushSinha1@users.noreply.github.com>
2023-09-10 19:09:20 +0530

    Deployment

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import router from './routes.js';
 import dotenv from 'dotenv'
 const app = express()
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://dropdowns-5qql.onrender.com/',
     credentials: true,
 }))
 dotenv.config()
